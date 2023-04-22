f42b64b30cd7ac0907efa9fe9c03b92e0677d02b
sasalate <113944640+sasalate@users.noreply.github.com>
2023-04-21 23:12:56 -0400

    Added contact validations and responsiveness

--- client/src/app/subscription-page/subscription-page.component.ts
+++ client/src/app/subscription-page/subscription-page.component.ts
@@ -1,15 +1,34 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'subscription-page',
   templateUrl: './subscription-page.component.html',
   styleUrls: ['./subscription-page.component.css']
 })
-export class SubscriptionPageComponent{
-
+export class SubscriptionPageComponent implements OnInit{
+  subForm: FormGroup;
   public showCardDetails = false; 
   public showPaypalDetails = false;
+
+  constructor(private formBuilder: FormBuilder,
+              private route: ActivatedRoute,
+              private router: Router) { }
+
+
+  ngOnInit() {
+    this.subForm = this.formBuilder.group({
+      street: ['', Validators.required],
+      town: ['', Validators.required],
+      city: ['', [Validators.required,]],
+      state: ['', Validators.required],
+      cardNumber: [''],
+      date: [''],
+      cvv: [''],
+      email: ['']
+    });
+  }
 
   handleCardClick() {
     this.showCardDetails = true;
@@ -19,6 +38,25 @@ export class SubscriptionPageComponent{
   handlePayPalClick() {
     this.showCardDetails = false;
     this.showPaypalDetails = true;
+    // this.subForm.controls['cardNumber'].setValidators([]);
+    // this.subForm.controls['date'].setValidators([]);
+    // this.subForm.controls['cvv'].setValidators([]);
+    // this.subForm.controls['email'].setValidators(Validators.required);
+  }
+
+  onSubmit() {
+    if (this.subForm.valid) {
+      // send message using a service or HTTP request
+      console.log(this.subForm.value);
+      this.subForm.reset();
+      alert('Form submitted successfully!');
+
+      this.router
+      .navigate(['/dashboard/subscription-plans']);
+    }
+     else {
+      alert('Please fill out all required fields.');
+    }
   }
  
 }
