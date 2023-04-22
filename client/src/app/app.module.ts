e84fed28240ffa6ce10784b7861352f42f66bc82
Kunal Thapa <thapa.ku@northeastern.edu>
2023-04-21 23:43:15 -0400

    final commit1

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import {HttpClientModule} from '@angular/common/http';
 import { DashboardModule } from "./dashboard/dashboard.module";
 
@@ -99,7 +99,8 @@ import { ContactUsComponent } from './contact-us/contact-us.component';
     BsDropdownModule.forRoot(),
     ButtonsModule.forRoot(),
     PaginationModule.forRoot(),
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    ReactiveFormsModule
   ],
   providers: [
     { provide: LocationStrategy, useClass: HashLocationStrategy },
